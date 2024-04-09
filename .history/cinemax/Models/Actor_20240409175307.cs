28f72cc503d0b198bfcd408ceae5e75d9344bc0c
Yasir Gaji <hellopleasantcodes@gmail.com>
2024-04-09 17:59:10 +0100

    cinemax project built with .net

---
+++ .history/cinemax/Models/Actor_20240409175307.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace cinemax.Models {
+  public class Actor {
+    public string ProfilePictureURL { get; set; }
+
+    public string FullName { get; set; }
+
+    public string Bio { get; set; }
+  }
+}
